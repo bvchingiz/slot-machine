d264a9d2371edb4f5a98526a749355802e96267b
bvchingiz <bvchingiz@gmail.com>
2023-05-26 14:31:34 +0600

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,15 @@ symbol_count  = {
 
 def get_slot_machine_spin(rows, cols, symbols):
     all_symbols = []
-    for symbol, symbol_count in 
+    for symbol, symbol_count in symbols.items():
+        for _ in range (symbol_count):
+            all_symbols.append(symbol)
+
+    columns = []
+    for col in range(cols):
+        column = []
+        for row in range(rows):
+            value = random.choice()
 
 
 
